43871af12079a44954c17eda50af04abb04c6ba9
Carlos Rodriguez <carlosluisrodri2007@hotmail.com>
2014-12-07 19:26:28 -0600

    Created the new qt project, close button ready, new game button "connected" in the main window code.
    Signed-off-by: Carlos Rodriguez <carlosluisrodri2007@hotmail.com>

---
+++ Sugoroku.pro
@@ -0,0 +1,25 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-12-07T16:14:19
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Sugoroku
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    MainWindow.cpp
+
+HEADERS  += \
+    MainWindow.h
+
+FORMS    += \
+    MainWindow.ui
+
+RESOURCES += \
+    assets/assets.qrc
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+  class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+  protected: // Data members
+	/** Points to an instance of the MainWindow design created by QtDesigner **/
+	Ui::MainWindow* ui;
+
+  public: // Public methods
+	/** Constructor **/
+	explicit MainWindow( QWidget* parent = 0);
+	/** Destructor **/
+	virtual ~MainWindow();
+
+  protected: // Utility methods
+	/** Set up connections that QtDesigner is unable to create **/
+	void setupConnections();
+	void displayNextQuestion();
+
+  protected slots:
+	/** Called when user asks for a new Trivia game **/
+	void actionNewTriggered();
+};
+
+#endif // MAINWINDOW_H
